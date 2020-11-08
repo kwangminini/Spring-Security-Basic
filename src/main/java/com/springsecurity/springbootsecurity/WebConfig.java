eef0f5cba9b8c966ad16c869ba00efe99f297700
kwangminini <rhkdals7362@gmail.com>
2020-11-08 16:33:53 +0900

    first commit

---
+++ src/main/java/com/springsecurity/springbootsecurity/WebConfig.java
@@ -0,0 +1,13 @@
+//package com.springsecurity.springbootsecurity;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/hello").setViewName("hello");
+//    }
+//}
---
+++ src/test/java/com/springsecurity/springbootsecurity/HomeControllerTest.java
@@ -0,0 +1,34 @@
+package com.springsecurity.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello")
+                    .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"));
+    }
+    @Test
+    public void my() throws Exception {
+        mockMvc.perform(get("/my"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("my"));
+    }
+
+}
